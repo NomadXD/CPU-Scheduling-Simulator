1b0d4a992c97046ee55fd7b370fbca02f656d740
Lahiru <lahirudesilva.17@cse.mrt.ac.lk>
2019-04-14 12:44:14 +0530

    Adding user inputs to tables implemented

--- app.js
+++ app.js
@@ -37,3 +37,108 @@ var renderPriorityForm = function(){
     $("#create-process-form").load("src/PriorityForm.html")
 }
 
+
+if(document.getElementById("btn-submit-index")){
+    document.querySelector("#btn-submit-index").addEventListener("click",function(e){
+        e.preventDefault()
+        console.log("Submitted")
+        let PID = document.getElementById("process-id-list-index").value
+        let arrivalTime = document.getElementById("arrival-time-index").value
+        let executeTime = document.getElementById("execute-time-index").value
+    
+        console.log(PID)
+        console.log(arrivalTime)
+        console.log(executeTime)
+    
+        let table = document.getElementById("process-table-index")
+        for (var i = 0, row; row = table.rows[i]; i++) {
+            //iterate through rows
+            //rows would be accessed using the "row" variable assigned in the for loop
+            if(row.cells[0].innerText==PID){
+                row.cells[1].innerText=arrivalTime
+                row.cells[2].innerText=executeTime
+            }
+    
+         }
+    })
+
+}else if(document.getElementById("btn-submit-FCFS")){
+    document.querySelector("#btn-submit-FCFS").addEventListener("click",function(e){
+        e.preventDefault()
+        console.log("Submitted")
+        let PID = document.getElementById("process-id-list-FCFS").value
+        let arrivalTime = document.getElementById("arrival-time-FCFS").value
+        let executeTime = document.getElementById("execute-time-FCFS").value
+    
+        console.log(PID)
+        console.log(arrivalTime)
+        console.log(executeTime)
+    
+        let table = document.getElementById("process-table-FCFS")
+        for (var i = 0, row; row = table.rows[i]; i++) {
+            //iterate through rows
+            //rows would be accessed using the "row" variable assigned in the for loop
+            if(row.cells[0].innerText==PID){
+                row.cells[1].innerText=arrivalTime
+                row.cells[2].innerText=executeTime
+            }
+    
+         }
+    })
+    
+}else if(document.getElementById("btn-submit-RR")){
+    document.querySelector("#btn-submit-RR").addEventListener("click",function(e){
+        e.preventDefault()
+        console.log("Submitted")
+        let PID = document.getElementById("process-id-list-RR").value
+        let arrivalTime = document.getElementById("arrival-time-RR").value
+        let executeTime = document.getElementById("execute-time-RR").value
+    
+        console.log(PID)
+        console.log(arrivalTime)
+        console.log(executeTime)
+    
+        let table = document.getElementById("process-table-RR")
+        for (var i = 0, row; row = table.rows[i]; i++) {
+            //iterate through rows
+            //rows would be accessed using the "row" variable assigned in the for loop
+            if(row.cells[0].innerText==PID){
+                row.cells[1].innerText=arrivalTime
+                row.cells[2].innerText=executeTime
+            }
+    
+         }
+    })
+}else if(document.getElementById("btn-submit-PR")){
+    document.querySelector("#btn-submit-PR").addEventListener("click",function(e){
+        e.preventDefault()
+        console.log("Submitted")
+        let PID = document.getElementById("process-id-list-PR").value
+        let arrivalTime = document.getElementById("arrival-time-PR").value
+        let executeTime = document.getElementById("execute-time-PR").value
+        let priority = document.getElementById("priority").value
+    
+        console.log(PID)
+        console.log(arrivalTime)
+        console.log(executeTime)
+    
+        let table = document.getElementById("process-table-PR")
+        for (var i = 0, row; row = table.rows[i]; i++) {
+            //iterate through rows
+            //rows would be accessed using the "row" variable assigned in the for loop
+            if(row.cells[0].innerText==PID){
+                row.cells[1].innerText=arrivalTime
+                row.cells[2].innerText=executeTime
+                row.cells[4].innerText=priority
+            }
+    
+         }
+    })
+}
+
+
+
+
+
+
+
---
+++ src/FCFS.js
@@ -0,0 +1,11 @@
+var addProcess = function(){
+
+    document.getElementById("btn-submit-FCFS").addEventListener("click",function(e){
+        e.preventDefault()
+        console.log("FCFS Submitted")
+    })
+
+
+
+
+}
